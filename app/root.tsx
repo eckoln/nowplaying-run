ab4954673bcbb5fb94687707c240efb3fef97cc6
Ahmet <rganned@gmail.com>
2022-11-20 21:23:13 +0300

    .

--- app/root.tsx
+++ app/root.tsx
@@ -27,10 +27,10 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Now Playing - The lightweight overlay tool for your live-streams",
+  title: "Now Playing - The Minimalistic Overlay for Livestreams",
   viewport: "width=device-width,initial-scale=1",
   description:
-    "Now Playing is an overlay service for live-streamers that serve up to overlay the currently track you are listening on Spotify.",
+    "Now Playing is an overlay service for livestreamers that serves up an overlay of the track you are listening to on Spotify.",
   keywords:
     "spotify now playing, spotify currently playing, now playing overlay, now playing",
 });
