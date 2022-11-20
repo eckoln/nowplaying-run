de5c6a096a49d58af4e39e53777c72357c55c174
Ahmet <rganned@gmail.com>
2022-11-20 15:16:24 +0300

    .

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,28 @@
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-screen flex-col items-center justify-center">
       <div className="container">
         <div className="space-y-4 text-center">
-          <h1 className="text-6xl font-bold leading-tight">
+          <h1 className="text-5xl font-bold leading-tight laptop:text-6xl">
             <span className="text-indigo-400">Hello world!</span> You're early,
             but <span className="text-green-400">no worries!</span>
           </h1>
           <p className="text-gray-400">
             The lightweight overlay tool for your live-streams.
           </p>
+          <footer className="text-gray-400">
+            <div className="container">
+              made by{" "}
+              <a
+                href="https://twitter.com/eckoln"
+                className="font-semibold transition-colors hover:text-green-400"
+                target="_blank"
+                rel="noreferrer"
+              >
+                @eckoln
+              </a>
+            </div>
+          </footer>
         </div>
       </div>
     </div>
