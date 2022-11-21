734321ed2f20eb2c84d8478bf58716bdf1be98ef
Ahmet <rganned@gmail.com>
2022-11-21 13:36:03 +0300

    edit auth hero text

--- app/components/AuthHeroHomePage.tsx
+++ app/components/AuthHeroHomePage.tsx
@@ -3,16 +3,16 @@ import Heading from "@/ui/Heading";
 
 export default function AuthHeroHomePage({ refId }: { refId: string }) {
   return (
-    <div className="flex flex-col items-center space-y-2 border-t border-white/20 pt-4">
+    <div className="flex flex-col items-center space-y-2 border-t border-white/10 pt-4">
       <Heading as="h4" variant="h4">
-        Hello eckoln! Your overlay url is here:
+        Hello eckoln! Your overlay URL is here:
       </Heading>
       <Button variant="copyToUrl" size="md">
         http://localhost:3000/overlays?ref={refId}
       </Button>
       <p className="text-sm">
-        Now, you have to few settings. Follow the directives at the bottom of
-        page.
+        You must now make a few adjustments. Follow the directives at the bottom
+        of the page.
       </p>
     </div>
   );
