049d98deb993a20422255b9075d9eaba43071af9
Ahmet <rganned@gmail.com>
2022-11-27 13:30:27 +0300

    add database env

--- src/utils/env.server.ts
+++ src/utils/env.server.ts
@@ -1,10 +1,11 @@
-import { str, envsafe, url } from "envsafe";
+import { envsafe, str, url } from "envsafe";
 
 export const env = envsafe({
   NODE_ENV: str({
     devDefault: "development",
     choices: ["development", "test", "production"],
   }),
+  DATABASE_URL: url(),
   SPOTIFY_CLIENT_ID: str(),
   SPOTIFY_CLIENT_SECRET: str(),
   SPOTIFY_CLIENT_REDIRECT_URI: url(),
